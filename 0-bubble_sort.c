265d7d677d852b437f60626748fd57a61d89d3e2
kayroyce <102330728+kayroyce@users.noreply.github.com>
2022-08-16 22:37:01 +0100

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,57 +1,26 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
- */
-void swap_ints(int *a, int *b)
-{
-		int tmp;
-
-		tmp = *a;
-		*a = *b;
-		*b = tmp;
-}
-
-
-/**
- * bubble_sort - sorts bubbly
- * @array: array to sort
- * @size: size
- *
+ * bubble_sort - the worst sorting algorithm
+ * @array: array to be sorted in place
+ * @size: size of array
  */
 void bubble_sort(int *array, size_t size)
 {
-		size_t i, newsize = size;
-		int swap;
+	size_t i, j;
+	int max;
 
-		if (array == NULL || size < 2)
-				return;
-
-		while (newsize)
+	for (i = size; i > 1; i--)
+	{
+		for (j = 0; j < i - 1; j++)
 		{
-				swap = 0;
-				for (i = 0; i < (size - 1); i++)
-				{
-						if (array[i] > array[i + 1])
-						{
-								swap_ints(&array[i], &array[i + 1]);
-								print_array(array, size);
-								swap = 1;
-						}
-				}
-				newsize--;
-				if (swap == 0)
-						break;
+			if (array[j] > array[j + 1])
+			{
+				max = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = max;
+				print_array(array, size);
+			}
 		}
-<<<<<<< HEAD
-		newsize--;
-		if (swap == 0)
-			break;
 	}
 }
-
-=======
-}
->>>>>>> 688b2119631691214486b38a37adec150ba6026a
